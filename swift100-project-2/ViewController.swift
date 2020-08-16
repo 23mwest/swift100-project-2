6ab2507bb1e27199cedfad9c2f31ea40b81e9dd3
Michael West <michaelwest@jakes-air.myfiosgateway.com>
2020-08-16 12:52:06 -0400

    New functionality for days 20 & 21: Displayed score, feedback for wrong answers, and a final score alert.

--- swift100-project-2/ViewController.swift
+++ swift100-project-2/ViewController.swift
@@ -13,17 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet var Button1: UIButton!
     @IBOutlet var Button2: UIButton!
     @IBOutlet var Button3: UIButton!
+    @IBOutlet var scoreLabel: UILabel!
     
     var countries = [String]()
     var score = 0
+    var questionsAsked = 0
     var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        
-        
-
+        scoreLabel.text = ""
         
         Button1.layer.borderWidth = 1
         Button2.layer.borderWidth = 1
@@ -31,24 +31,57 @@ class ViewController: UIViewController {
         Button1.layer.borderColor = UIColor.lightGray.cgColor
         Button2.layer.borderColor = UIColor.lightGray.cgColor
         Button3.layer.borderColor = UIColor.lightGray.cgColor
-        
+
         askQuestion()
 
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        if(questionsAsked != 0){
+            scoreLabel.text = "\(score)/\(questionsAsked)"
+        }
+        
         countries.shuffle()
+        
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        title = (countries[correctAnswer].uppercased())
+        
         
         Button1.setImage(UIImage(named: countries[0]), for: .normal)
         Button2.setImage(UIImage(named: countries[1]), for: .normal)
         Button3.setImage(UIImage(named: countries[2]), for: .normal)
+        questionsAsked += 1
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message : String
 
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            message = "Your score is now: "
+            score += 1
+        } else {
+            title = "Wrong"
+            message = "That is the flag of \(countries[sender.tag].uppercased()). Your score is now: "
+        }
+        
+        if(questionsAsked == 10){
+            title = "Final Score"
+            message = "Your final score is: "
+        }
+        
+        let ac = UIAlertController(title: title, message: "\(message) \(score)/\(questionsAsked)", preferredStyle: .alert)
+        
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
 }
 
